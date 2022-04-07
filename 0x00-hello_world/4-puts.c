b2544b5fd65951062336d189df9b78faf400513f
Wairioko <charlesmungai5@gmail.com>
2022-04-07 01:01:04 -0700

    a C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *
+ *main - Entrypoint
+ *
+ *Return - Always 0 (Success)
+ *
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return 0;
+}
