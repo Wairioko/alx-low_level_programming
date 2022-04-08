af87912bf700ed727a758b701d27edbb97039602
Wairioko <charlesmungai5@gmail.com>
2022-04-08 05:11:20 -0700

    printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - print letter in alphabet lowercase
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for(i = 0; i < 26; i++)
+{
+putchar(aplh[i]);
+}
+putchar('\n');
+Return (0);
+}
