27027d3dee601aea0ab30c04d06c49f1080cca00
Wairioko <charlesmungai5@gmail.com>
2022-04-12 11:35:58 -0700

    Adding 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,14 +13,13 @@ if (n > 0)
 return(1);
 _putchar(43);
 }
-else if ((n = 0))
+else if ((n < 0))
 {
+  return(-1);
+_putchar(45);
+}
+else{
 return(0);
 _putchar(48);
 }
-else{
-return(-1);
-_putchar(45);
-}
- _putchar('\n');
 }
