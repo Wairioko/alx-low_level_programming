496e998b1dec215eb0ece815ab78e7c52eb0bcdf
Wairioko <charlesmungai5@gmail.com>
2022-04-12 12:17:05 -0700

    Adding 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_last_digit - prints last
+*digit of a number
+*@i - interger value ascii
+*Return: value of last digit
+*/
+int print_last_digit(int i)
+{
+int l;
+l = i % 10;
+if (l < 0)
+{
+_putchar(-l + 48);
+return (-l);
+}
+else
+{
+_putchar(l + 48);
+return (l);
+}
+}
