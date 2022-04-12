6575dea5d3ee6f275c0ff1632c79e4df7ab5ef2e
Wairioko <charlesmungai5@gmail.com>
2022-04-12 11:26:35 -0700

    Adding 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_sign - prints sign for numbers
+*Returns: rerurns 1 and + if n is greater than zero
+*0 and prints 0 if n is zero
+*-1 and prints - if n is less than zero
+*@n: The input number as an integer.
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+return(1);
+_putchar(43);
+}
+else if (n = 0)
+{
+return(0);
+_putchar(48);
+}
+else{
+return(-1);
+_putchar(45);
+}
+}
