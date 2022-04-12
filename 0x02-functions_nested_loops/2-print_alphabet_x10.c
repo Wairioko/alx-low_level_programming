3ca1769869cbd73a636e1db97148964fb6e08ece
Wairioko <charlesmungai5@gmail.com>
2022-04-12 06:21:39 -0700

    printig letter 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,13 @@
 */
 void print_alphabet_x10(void)
 {
-  int a, b;
-
-  for (a = 0; a <= 9; a++)
-    {
-      for (b = 97; b <= 122; b++)
-	{
-	  _putchar(b);
-	}
-      _putchar('\n');
-    }
+int a, b;
+for (a = 0; a <= 9; a++)
+{
+for (b = 97; b <= 122; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}
 }
