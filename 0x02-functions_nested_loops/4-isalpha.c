c510217591721849ee4246513321508a13689ab5
Wairioko <charlesmungai5@gmail.com>
2022-04-12 11:15:37 -0700

    Adding 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-*_isalpha - checks for alphabetic character
-* Return: 1 if c is a letter, lowercase or uppercase
+* _isalpha - checks for alphabetic character
+*Return: 1 if c is a letter, lowercase or uppercase
 *0 if otherwise
 *@c: The character in ASCII code
 */
-int _isaplha(int c)
+int _isalpha(int c)
 {
 if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 {
