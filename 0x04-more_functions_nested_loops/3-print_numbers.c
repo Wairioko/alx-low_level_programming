dabcfa6c75eb30932c9d4d04e96915e004770400
Wairioko <charlesmungai5@gmail.com>
2022-04-14 02:44:06 -0700

    Adding 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_numbers - prits nums betwen 0-9
+*
+*Return: Alway 0
+*/
+void print_numbers(void)
+{
+for (i = 0; i < 10; i++)
+{
+_putchar("%d",i);
+}
+_putchar('\n'
+}
