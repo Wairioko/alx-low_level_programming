7daabd4bfc88ed9d202e11e67167ffad86966e1a
Wairioko <charlesmungai5@gmail.com>
2022-04-14 03:16:25 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*more_numbers - print numbers 0-14 10 times
+*/
+void more_numbers(void)
+{
+int ch;
+int i;
+do {
+for (ch = 48; ch < 0E; ch++)
+{
+_putchar(ch);
+}
+while (i <= 10);
+}
