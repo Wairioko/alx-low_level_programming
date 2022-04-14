5b1c6746a3ca74a4f0dacf251fb5c8fa232702c1
Wairioko <charlesmungai5@gmail.com>
2022-04-14 00:54:45 -0700

    Addig 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,13 +5,13 @@
 */
 int main(void)
 {
-//int i;
+int i;
 printf("Infinite loop incoming :(\n");
-//i = 0;
+i = 0;
 //while (i < 10)
-//{
-//putchar(i);
-//}
+{
+putchar(i);
+}
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
