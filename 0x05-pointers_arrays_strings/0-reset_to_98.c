1e9dcfc8dd04aa522899e85627e8641dd462cb3e
Wairioko <charlesmungai5@gmail.com>
2022-04-20 01:06:52 -0700

    Adding files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*void reset_to_98 - takes a pointer to an int as
+* parameter and updates the 
+*value it points to to 98
+*@n: input integer.*
+*Return - Always 0 (success)
+*/
+void reset_to_98(int *n);
+{
+*n = 98;
+}
