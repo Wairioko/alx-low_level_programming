45571dea2dd07d6dbb769a464df5492cd26b021f
Wairioko <charlesmungai5@gmail.com>
2022-04-20 01:49:33 -0700

    Addig 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_rev - prits string in reverse
+*
+*@s: string
+*Return - Always 0 (success)
+*/
+void print_rev(char *s)
+{
+int count = 0;
+while (count => 0)
+{
+if (s[count] == '\0')
+break;
+count++;
+}
+for (count--; count >= 0; count--)
+_putchar(s[count]);
+_putchar('\n');
+}
