be185a28958e23f206f67ec13db0668880233e7e
Wairioko <charlesmungai5@gmail.com>
2022-04-24 09:13:27 -0700

    Adding 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 int main(void)
 {
@@ -14,7 +15,7 @@ p = &n;
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-p[2] = 98;
+p[5] = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
