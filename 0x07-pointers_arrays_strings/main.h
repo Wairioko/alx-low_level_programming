9e47090700fc47ab310ac47d72beb6a2a19137b2
Wairioko <charlesmungai5@gmail.com>
2022-04-26 07:22:53 -0700

    Change to main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef __MAIN.H__
-#define __MAIN.H__
+#ifndef __MAIN_H__
+#define __MAIN_H__
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
