3bf8f641c486cda98fd8d5c2d301a0605dd08bea
Wairioko <charlesmungai5@gmail.com>
2022-05-06 04:17:55 -0700

    Adding 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,36 +1,36 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
-* alloc_grid - returns a pointer to a 2 dimensional array of integers.
-* @width: width of the array.
-* @height: height of the array.
+* alloc_grid - function returns a pointer to a 2 dimensional array of
+* integers.
+* @width: colums.
+* @height: rows.
 *
-* Return: pointer of an array of integers
+* Return: Pointer to 2d array.
 */
 int **alloc_grid(int width, int height)
 {
-int **gridout;
-int i, j;
-if (width < 1 || height < 1)
+int **arr;
+int r, c, i;
+if (width <= 0 || height <= 0)
 return (NULL);
-gridout = malloc(height * sizeof(int *));
-if (gridout == NULL)
-{
-free(gridout);
+arr = (int **)malloc(sizeof(*arr) * height);
+if (arr == NULL)
 return (NULL);
-}
 for (i = 0; i < height; i++)
-gridout[i] = malloc(width * sizeof(int));
-if (gridout[i] == NULL)
 {
-for (i--; i >= 0; i--)
-free(gridout[i]);
-free(gridout);
+arr[i] = (int *)malloc(sizeof(int) * width);
+if (arr[i] == NULL)
+{
+while (i--)
+free(arr[i]);
+free(arr);
 return (NULL);
 }
 }
-for (i = 0; i < height; i++)
-for (j = 0; j < width; j++)
-gridout[i][j] = 0;
-return (gridout);
+for (r = 0; r < height; r++)
+for (c = 0; c < width; c++)
+arr[r][c] = 0;
+return (arr);
 }
