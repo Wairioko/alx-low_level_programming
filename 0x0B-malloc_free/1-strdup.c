7246ecd1307e9bdbb0918443f1278a3ea6fae69f
Wairioko <charlesmungai5@gmail.com>
2022-05-06 04:41:10 -0700

    Adding 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,8 +12,10 @@ char *strout;
 unsigned int i, j;
 if (str == NULL)
 return (NULL);
-for (i = 0; str[i] != '\0'; i++);
-strout = (char *)malloc(sizeof(char) * (i + 1));
+for (i = 0; str[i] != '\0'; i++)
+;
+strout = (char *)malloc(sizeof(char) * (i + 1))
+;
 if (strout == NULL)
 return (NULL);
 for (j = 0; j <= i; j++)
