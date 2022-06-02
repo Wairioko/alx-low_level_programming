84d32795bed59ea1d9a311d672217f2ed2dee1db
Wairioko <charlesmungai5@gmail.com>
2022-06-02 07:43:37 -0700

    Corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,8 +11,8 @@ int fd;
 int nletters;
 int rwr;
 if (!filename)
-return (-1)
-fd  = open(filename, 0_CREAT | 0_WRONLY | 0_TRUNC, 0600);
+return (-1);
+fd  = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
 if (!text_content)
@@ -21,7 +21,7 @@ for (nletters = 0; text_content[nletters]; nletters++)
 ;
 rwr = write(fd, text_content, nletters);
 if (rwr == -1)
-return (-1)
+return (-1);
 close(fd);
 return (1);
 }
