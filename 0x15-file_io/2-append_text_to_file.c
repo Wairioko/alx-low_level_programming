96121b161e310b6c3cf10465318587a255affe51
Wairioko <charlesmungai5@gmail.com>
2022-06-02 07:01:50 -0700

    Adding file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,13 +11,14 @@ int fd;
 int nletters;
 int rwr;
 if (!filename)
-return (-1)
+return (-1);
 fd = open(filename, 0_WRONLY | 0_APPEND);
 if (fd == -1)
 return (-1);
 if (text_content)
 {
 for (nletters = 0; text_content[nletters]; nletters++)
+;
 rwr = write(fd, text_content, letters);
 if (rwr == -1)
 return (-1);
