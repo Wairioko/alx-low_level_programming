110eb0a213ca5f60ce5284fabbea78571ee4a21e
Wairioko <charlesmungai5@gmail.com>
2022-06-02 07:33:36 -0700

    Adding 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,13 +12,12 @@ int nletters;
 int rwr;
 if (!filename)
 return (-1);
-fd = open(filename, 0_WRONLY | 0_APPEND);
+fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
 if (text_content)
 {
-for (nletters = 0; text_content[nletters]; nletters++)
-;
+for (nletters = 0; text_content[nletters]; nletters++);
 rwr = write(fd, text_content, letters);
 if (rwr == -1)
 return (-1);
