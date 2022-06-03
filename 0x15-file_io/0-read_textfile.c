9463719f0e6cbccfda2927ab1fe1ea1db1520937
Wairioko <charlesmungai5@gmail.com>
2022-06-03 15:52:59 -0700

    Adding file 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
 *@letters:numbers of letters printed.
 *Return:numbers of letters printed. It fails, returns 0.
 */
-ssize_t read_textfile (const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 int fd;
 ssize_t nrd, nwr;
